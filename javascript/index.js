704f53a18090971ce76144015b3ce13f1f639931
KMIRNS <igormartins.mcz@gmail.com>
2022-04-02 16:23:08 -0300

    Fase 02
    Operadores relacionais e comparativos

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,11 @@
-const aluno01 = "Igor"
-const notaAluno01 = 9.8
+const idade = 17
 
-const aluno02 = "Aldriele"
-const notaAluno02 = 10
+if(idade >= 18){
+  console.log('Deixar entrar!')
+}else{
+  console.log('Proibir entrada!')  
+}
 
-const aluno03 = "Beatriz"
-const notaAluno03 = 2
-
-
-const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
-
-if(media > 5){
-  console.log(`Você foi aprovado, parabéns!`)
-}else {
-  console.log(`Você foi reprovado, sinto muito!`)
+if(idade === 17){
+  console.log('Volte quando fizer 18 anos.')
 }
