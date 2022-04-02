209d76bdf579e5e5d07c336e52c53b3e341b9c40
KMIRNS <igormartins.mcz@gmail.com>
2022-04-02 16:05:47 -0300

    Fase 02
    Condicionais

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,17 @@
 const aluno01 = "Igor"
-const aluno02 = "Aldriele"
-const aluno03 = "Beatriz"
-
 const notaAluno01 = 9.8
+
+const aluno02 = "Aldriele"
 const notaAluno02 = 10
+
+const aluno03 = "Beatriz"
 const notaAluno03 = 2
+
 
 const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
 
-console.log(media)
+if(media > 5){
+  console.log(`Você foi aprovado, parabéns!`)
+}else {
+  console.log(`Você foi reprovado, sinto muito!`)
+}
