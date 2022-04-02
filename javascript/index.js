27f1040fd389fef991abb4d90c9414e96a2ca39c
KMIRNS <igormartins.mcz@gmail.com>
2022-04-02 15:58:46 -0300

    Fase 02
    Fazendo cálculos com JavaScript

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,11 @@
 const aluno01 = "Igor"
 const aluno02 = "Aldriele"
+const aluno03 = "Beatriz"
 
 const notaAluno01 = 9.8
 const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+console.log(media)
