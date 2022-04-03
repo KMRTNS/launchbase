eaa2da8b1b44296855f76559c1a662e7103d4f89
KMIRNS <igormartins.mcz@gmail.com>
2022-04-03 19:38:28 -0300

    Fase 02
    Objetos

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,22 @@
-const idade = 17
+const aluno01 = {
+  nome: "Igor",
+  nota: 9.8
+}
 
-if(!(idade >= 18) || idade === 17){
-  console.log('Proibir entrada!')  
+const aluno02 = {
+  nome: "Aldriele",
+  nota: 10
+}
+
+const aluno03 = {
+  nome: "Beatriz",
+  nota: 2
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+if(media > 5){
+  console.log(`A media foi de ${media}, parabéns!`)
 }else{
-  console.log('Deixar entrar!')
+  console.log(`A media foi de ${media}, sinto muito!`)
 }
