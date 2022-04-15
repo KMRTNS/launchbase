841a6633627a4b67f67cc36f8509b2b4f9201009
Igor <igormartins.mcz@gmail.com>
2022-04-15 09:01:53 -0300

    FASE 01
    Organização, padronização e escrita

--- fase_01/intro-pro-web/index.js
+++ fase_01/intro-pro-web/index.js
@@ -38,47 +38,47 @@ const alunosDaTurmaB = [
 
 function calculaMedia(alunos) {
   soma = 0
-  for(let i = 0; i < alunos.length; i++) {
+  for (let i = 0; i < alunos.length; i++) {
     soma = soma + alunos[i].nota
   }
   const media = soma / alunos.length
   return media
 }
 
-const media1 = calculaMedia(alunosDaTurmaA)
-const media2 = calculaMedia(alunosDaTurmaB)
-
 function enviaMensagem(media, turma) {
-  if(media > 5){
+  if (media > 5) {
     console.log(`A media da ${turma} foi de ${media}, parabéns!`)
-  }else{
+  } else {
     console.log(`A media da ${turma} foi de ${media}, sinto muito!`)
   }
 }
 
-enviaMensagem(media1, 'turma A')
-enviaMensagem(media2, 'turma B')
-
 function marcarComoReprovado(aluno) {
   aluno.reprovado = false
-  if(aluno.nota <= 5) {
+  if (aluno.nota <= 5) {
     aluno.reprovado = true
   }
 }
 
 function enviarMensagemReprovado(aluno) {
-  if(aluno.reprovado) {
+  if (aluno.reprovado) {
     console.log(`O aluno ${aluno.nome} está reprovado!`)
   }
 }
 
-function alunoReprovado(alunos) {
+function alunosReprovado(alunos) {
   console.table(alunos)
-  for(let aluno of alunos ) {
+  for (let aluno of alunos) {
     marcarComoReprovado(aluno)
     enviarMensagemReprovado(aluno)
   }
 }
 
-alunoReprovado(alunosDaTurmaA)
-alunoReprovado(alunosDaTurmaB)
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+enviaMensagem(media1, 'turma A')
+enviaMensagem(media2, 'turma B')
+
+alunosReprovado(alunosDaTurmaA)
+alunosReprovado(alunosDaTurmaB)
