32e3f9252473f2d11a4dfe58e71b4bc5d405dcac
Igor <igormartins.mcz@gmail.com>
2022-04-15 07:45:12 -0300

    FASE 01
    Funções e métodos

--- fase_01/intro-pro-web/index.js
+++ fase_01/intro-pro-web/index.js
@@ -1,4 +1,4 @@
-const alunos = [
+const alunosDaTurmaA = [
   {
     nome: "Igor",
     nota: 9.8
@@ -13,10 +13,35 @@ const alunos = [
   }
 ]
 
-const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+const alunosDaTurmaB = [
+  {
+    nome: "Junio",
+    nota: 6
+  },
+  {
+    nome: "Alisson",
+    nota: 7
+  },
+  {
+    nome: "Mayk",
+    nota: 2
+  }
+]
 
-if(media > 5){
-  console.log(`A media foi de ${media}, parabéns!`)
-}else{
-  console.log(`A media foi de ${media}, sinto muito!`)
+function calculaMedia(alunos) {
+  return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 }
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma) {
+  if(media > 5){
+    console.log(`A media da ${turma} foi de ${media}, parabéns!`)
+  }else{
+    console.log(`A media da ${turma} foi de ${media}, sinto muito!`)
+  }
+}
+
+enviaMensagem(media1, 'turma A')
+enviaMensagem(media2, 'turma B')
