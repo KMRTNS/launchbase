08b914d1cdf4311697dcd3572f25fe1d2b8a12e9
Igor <igormartins.mcz@gmail.com>
2022-04-16 22:56:15 -0300

    FASE 01
    Desafio 3-2: Arquivos nunjucks e dados dinâmicos

---
+++ fase_01/intro-back-end/challenges/dataset/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "id": "do-back-ao-mobile-de-onde-surgiu-a-programacao-fullstack/",
+        "img_src": "https://blog.rocketseat.com.br/content/images/size/w2000/2021/12/Como_ser_um_dev_fullstack.png",
+        "title": "Do back ao mobile: de onde surgiu a programação fullstack",
+        "author": "Felipe Buzzi",
+        "time": "1h"
+    },
+    {
+        "id": "primeira-next-level-week/",
+        "img_src": "https://blog.rocketseat.com.br/content/images/size/w2000/2020/06/rocketseat_next_level_week.jpg",
+        "title": "Primeira Next Level Week #NLW",
+        "author": "Thiago Marinho",
+        "time": "1h30m"
+    },
+    {
+        "id": "5-dicas-para-uma-carreira-solida-como-programador-a/",
+        "img_src": "https://blog.rocketseat.com.br/content/images/size/w2000/2020/06/dicas-carreira-solida-programador-rocketseat.png",
+        "title": "5 dicas para uma carreira sólida como programador(a)",
+        "author": "Thiago Marinho",
+        "time": "40m"
+    }
+]
