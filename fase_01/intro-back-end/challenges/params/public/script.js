e3e30ab3273146bb89d17a14d434a14f58b57627
Igor <igormartins.mcz@gmail.com>
2022-04-17 10:20:55 -0300

    FASE 01
    Desafio 3-3: Página de descrição do curso

---
+++ fase_01/intro-back-end/challenges/params/public/script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+  card.addEventListener('click', function(){
+    
+    const pageId = card.getAttribute('id')
+
+    window.location.href = `/${pageId}`
+
+  })
+}
---
+++ fase_01/intro-back-end/challenges/params/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const post = require('./data')
+
+const server = express()
+
+server.use(express.static("public"))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false
+})
+
+server.get("/", function(req, res){
+    return res.render("courses", { dataset: post})
+})
+
+server.get("/about", function(req, res){
+
+    const dataset = {
+        logo: "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
+        title: "Rocketseat",
+        description: 'Lorem ipsum dolor sit amet consectetur, <a href="">adipisicing elit</a>.',
+        list: [
+            {technology: "HTML"},
+            {technology: "JavaScript"},
+            {technology: "CSS"}
+        ],
+        links: [
+            {url: "/", urlName: "GitHub"},
+            {url: "/", urlName: "Instagram"},
+            {url: "/", urlName: "Facebook"}
+        ]
+    }
+
+    return res.render("about", {data: dataset})
+})
+
+server.get("/:id", function(req, res){
+    
+    const id = req.params.id
+
+    return res.render("content", {id})
+
+})
+
+server.listen(5000, function(){
+    console.log("started")
+})
