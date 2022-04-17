44407cb7355936e4e2f530a60c355b850c964f0e
Igor <igormartins.mcz@gmail.com>
2022-04-16 22:28:49 -0300

    FASE 01
    Passando dados do Back para o Front

--- fase_01/intro-back-end/data.js
+++ fase_01/intro-back-end/data.js
@@ -4,41 +4,35 @@ module.exports = [
     title: "Formulário animado com JS puro e CSS animation",
     price: "Free",
     time: "57 min",
-    featured: true
   },
   {
     id: "HN1UjzRSdBk",
     title: "Desenvolvendo o CSS Grid na prática",
     price: "Free",
     time: "36 min",
-    featured: false
   },
   {
     id: "rAzHvYnQ8DY",
     title: "Array: Higher Order Functions",
     price: "Free",
     time: "54 min",
-    featured: true
   },
   {
     id: "ghTrp1x_1As",
     title: "O que é API? REST e RESTful?",
     price: "Free",
     time: "33 min",
-    featured: false
   },
   {
     id: "GSqR2i-Pq6o",
     title: "Desvendando a variável this no javascript",
     price: "Free",
     time: "48 min",
-    featured: false
   },
   {
     id: "2alg7MQ6_sI",
     title: "Como usar Git e Github na prática: Guia para iniciantes",
     price: "Free",
     time: "33 min",
-    featured: true
   }
 ]
