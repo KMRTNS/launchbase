a5fc9955e36907043a94146f91920649d007c862
Igor <igormartins.mcz@gmail.com>
2022-04-16 22:53:59 -0300

    FASE 01
    Atualizando página sobre

--- fase_01/intro-back-end/server.js
+++ fase_01/intro-back-end/server.js
@@ -9,11 +9,23 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get('/', function(req, res) {
-  return res.render('about')
+  const about = {
+    avatar_url: "https://avatars.githubusercontent.com/u/102094427?v=4",
+    name: "Igor Martins",
+    role: "Estudante de Programação",
+    description: 'Estudante - Focado em aprender programação na <a href="https://app.rocketseat.com.br" target="_blank">Rocketseat</a>',
+    links: [
+      {url: "/", name: "GitHub"},
+      {url: "/", name: "Twitter"},
+      {url: "/", name: "LinkedIn"}
+    ]
+  }
+  return res.render('about', { about })
 })
 
 server.get('/portfolio', function(req, res) {
