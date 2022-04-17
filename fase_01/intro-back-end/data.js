1f5dd3a3919218370739e74c645ba16103acf4e6
Igor <igormartins.mcz@gmail.com>
2022-04-16 23:02:01 -0300

    FASE 01
    Adicionando vídeos em destaque

--- fase_01/intro-back-end/data.js
+++ fase_01/intro-back-end/data.js
@@ -4,35 +4,41 @@ module.exports = [
     title: "Formulário animado com JS puro e CSS animation",
     price: "Free",
     time: "57 min",
+    featured: false
   },
   {
     id: "HN1UjzRSdBk",
     title: "Desenvolvendo o CSS Grid na prática",
     price: "Free",
     time: "36 min",
+    featured: true
   },
   {
     id: "rAzHvYnQ8DY",
     title: "Array: Higher Order Functions",
     price: "Free",
     time: "54 min",
+    featured: false
   },
   {
     id: "ghTrp1x_1As",
     title: "O que é API? REST e RESTful?",
     price: "Free",
     time: "33 min",
+    featured: true
   },
   {
     id: "GSqR2i-Pq6o",
     title: "Desvendando a variável this no javascript",
     price: "Free",
     time: "48 min",
+    featured: false
   },
   {
     id: "2alg7MQ6_sI",
     title: "Como usar Git e Github na prática: Guia para iniciantes",
     price: "Free",
     time: "33 min",
+    featured: true
   }
 ]
