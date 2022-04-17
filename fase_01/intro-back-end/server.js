7c1b6323b72234d2813150d3b9934733a464b44d
Igor <igormartins.mcz@gmail.com>
2022-04-16 23:45:07 -0300

    FASE 01
    Filtrando elementos do array de vídeos

--- fase_01/intro-back-end/server.js
+++ fase_01/intro-back-end/server.js
@@ -35,7 +35,18 @@ server.get('/portfolio', function(req, res) {
 
 server.get('/video', function(req, res) {
   const id = req.query.id
-  return res.send(id)
+
+  const video = videos.find(function(video) {
+    if(video.id == id) {
+      return true
+    }
+  })
+
+  if(!video) {
+    return res.send('video is not found!')
+  }
+
+  return res.render('video', { video })
 })
 
 server.listen(5000, function() {
