2b92383298ea98b9008492be6f4e42eaf8a3a9eb
Igor <igormartins.mcz@gmail.com>
2022-04-18 19:54:55 -0300

    FASE 02
    Trabalhando com formulários HTML

--- fase_02/gym-control/routes.js
+++ fase_02/gym-control/routes.js
@@ -9,8 +9,16 @@ routes.get('/instructors', function(req, res) {
   return res.render('instructors/index')
 })
 
+routes.get('/instructors/create', function(req, res) {
+  return res.render('instructors/create')
+})
+
 routes.get('/members', function(req, res) {
   return res.send('members')
+})
+
+routes.post('/instructors', function(req, res) {
+  return res.send('Recebido!')
 })
 
 module.exports = routes
