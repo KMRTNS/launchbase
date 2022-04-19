425ff009906f94c1821b5c9eb78f71ba6f5f1dbe
Igor <igormartins.mcz@gmail.com>
2022-04-18 22:09:40 -0300

    FASE 02
    Separando as funções das rotas

--- fase_02/gym-control/routes.js
+++ fase_02/gym-control/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function(req, res) {
   return res.redirect('instructors')
@@ -17,14 +18,6 @@ routes.get('/members', function(req, res) {
   return res.send('members')
 })
 
-routes.post('/instructors', function(req, res) {
-  const keys = Object.keys(req.body)
-  for(key of keys) {
-    if(req.body[key] == '') {
-      return res.send('Please, fill all fields!')
-    }
-  }
-  return
-})
+routes.post('/instructors', instructors.post)
 
 module.exports = routes
