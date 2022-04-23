901c18d8f40b23498d3e5423087b9780c47a87f2
Igor <igormartins.mcz@gmail.com>
2022-04-22 22:24:24 -0300

    FASE 02
    Conhecendo o Spread Operator do Javascript

--- fase_02/gym-control/instructors.js
+++ fase_02/gym-control/instructors.js
@@ -13,9 +13,15 @@ exports.show = function(req, res) {
     return res.send('Instructor not found!')
   }
 
-  return res.render('instructors/show', { 
-    instructor: foundInstructor 
-  })
+  const instructor = {
+    ...foundInstructor,
+    age: "",
+    gender: "",
+    services: "",
+    created_at: ""
+  }
+
+  return res.render('instructors/show', { instructor })
 }
 
 exports.post = function(req, res) {
